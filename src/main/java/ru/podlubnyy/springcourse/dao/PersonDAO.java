0e187732cbebdcb0c316b7101f7d5470bf153421
Alexey <80638551+alexcypriiz@users.noreply.github.com>
2021-06-25 00:22:44 +0400

    chore: add variable: age, email (#4)

--- src/main/java/ru/podlubnyy/springcourse/dao/PersonDAO.java
+++ src/main/java/ru/podlubnyy/springcourse/dao/PersonDAO.java
@@ -13,11 +13,11 @@ public class PersonDAO {
 
     {
         people = new ArrayList<>();
-        people.add(new Person(++PEOPLE_COUNT, "Vlad"));
-        people.add(new Person(++PEOPLE_COUNT, "Alex"));
-        people.add(new Person(++PEOPLE_COUNT, "German"));
-        people.add(new Person(++PEOPLE_COUNT, "Chris"));
-        people.add(new Person(++PEOPLE_COUNT, "Samara"));
+        people.add(new Person(++PEOPLE_COUNT, "Vlad", 23, "themetronome@gmail.com"));
+        people.add(new Person(++PEOPLE_COUNT, "Alex", 24, "cypriiz@yandex.ru"));
+        people.add(new Person(++PEOPLE_COUNT, "German", 22, "sblgerman@yandex.ru"));
+        people.add(new Person(++PEOPLE_COUNT, "Chris", 37, "chrispratt@gmail.com"));
+        people.add(new Person(++PEOPLE_COUNT, "Samara", 29,"missladymylove@gmail.com"));
 
     }
 
@@ -37,6 +37,8 @@ public class PersonDAO {
     public void update(int id, Person updatePerson) {
         Person personToBeUpdated = show(id);
         personToBeUpdated.setName(updatePerson.getName());
+        personToBeUpdated.setAge(updatePerson.getAge());
+        personToBeUpdated.setEmail(updatePerson.getEmail());
     }
 
     public void delete(int id) {
