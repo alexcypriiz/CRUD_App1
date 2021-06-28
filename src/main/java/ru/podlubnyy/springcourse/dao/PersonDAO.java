244a987cb21145239e2648c60c77bff0c0defbf3
alexcypriiz <alexey.podlubny@yandex.ru>
2021-06-29 01:08:45 +0400

    refactor: Use JDBC API for connect Java and PostgreSQL database

--- src/main/java/ru/podlubnyy/springcourse/dao/PersonDAO.java
+++ src/main/java/ru/podlubnyy/springcourse/dao/PersonDAO.java
@@ -3,45 +3,87 @@ package ru.podlubnyy.springcourse.dao;
 import org.springframework.stereotype.Component;
 import ru.podlubnyy.springcourse.models.Person;
 
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
 @Component
 public class PersonDAO {
     private static int PEOPLE_COUNT;
-    private List<Person> people;
 
-    {
-        people = new ArrayList<>();
-        people.add(new Person(++PEOPLE_COUNT, "Vlad", 23, "themetronome@gmail.com"));
-        people.add(new Person(++PEOPLE_COUNT, "Alex", 24, "cypriiz@yandex.ru"));
-        people.add(new Person(++PEOPLE_COUNT, "German", 22, "sblgerman@yandex.ru"));
-        people.add(new Person(++PEOPLE_COUNT, "Chris", 37, "chrispratt@gmail.com"));
-        people.add(new Person(++PEOPLE_COUNT, "Samara", 29,"missladymylove@gmail.com"));
+    private static final String URL = "jdbc:postgresql://localhost:5432/first_db";
+    private static final String USERNAME = "cypriiz_comp";
+    private static final String PASSWORD = "cypriiz";
 
+    private static Connection connection;
+
+    static {
+        try {
+            Class.forName("org.postgresql.Driver");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
     }
 
     public List<Person> index() {
+        List<Person> people = new ArrayList<>();
+
+        try {
+            Statement statement = connection.createStatement();
+            String SQL = "SELECT * FROM Person";
+            ResultSet resultSet = statement.executeQuery(SQL);
+
+            while (resultSet.next()) {
+                Person person = new Person();
+
+                person.setId(resultSet.getInt("id"));
+                person.setName(resultSet.getString("Name"));
+                person.setEmail(resultSet.getString("Email"));
+                person.setAge(resultSet.getInt("age"));
+
+                people.add(person);
+            }
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
         return people;
     }
 
     public Person show(int id) {
-        return people.stream().filter(person -> person.getId() == id).findAny().orElse(null);
+//        return people.stream().filter(person -> person.getId() == id).findAny().orElse(null);
+        return null;
     }
 
     public void save(Person person) {
-        person.setId(++PEOPLE_COUNT);
-        people.add(person);
+//        person.setId(++PEOPLE_COUNT);
+//        people.add(person);
+
+        try {
+            Statement statement = connection.createStatement();
+            String SQL = "INSERT INTO Person VALUES(" + 1 + ",'" + person.getName() +
+                    "'," + person.getAge() + ",'" + person.getEmail() + "')";
+//            INSERT INTO Person VALUES(1, 'Name', age, 'email')
+
+            statement.executeUpdate(SQL);
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
     }
 
     public void update(int id, Person updatePerson) {
-        Person personToBeUpdated = show(id);
-        personToBeUpdated.setName(updatePerson.getName());
-        personToBeUpdated.setAge(updatePerson.getAge());
-        personToBeUpdated.setEmail(updatePerson.getEmail());
+//        Person personToBeUpdated = show(id);
+//        personToBeUpdated.setName(updatePerson.getName());
+//        personToBeUpdated.setAge(updatePerson.getAge());
+//        personToBeUpdated.setEmail(updatePerson.getEmail());
     }
 
     public void delete(int id) {
-        people.removeIf(p -> p.getId() == id);
+//        people.removeIf(p -> p.getId() == id);
     }
 }
